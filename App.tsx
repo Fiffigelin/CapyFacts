858d76bbfae67229426da76263c865752d4914b1
Elin Nyman <elin.nyman87@gmail.com>
2023-09-24 19:57:39 +0200

    Seperated RootTabsNavigator from app.tsx - a la friday lecture

--- App.tsx
+++ App.tsx
@@ -1,53 +1,15 @@
-import { MaterialIcons } from "@expo/vector-icons";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import HomeScreen from "./screens/HomeScreen";
-import FavoritesScreen from "./screens/favorites/FavoritesScreen";
-
-type RootTabScreens = {
-  HomeTab: undefined;
-  FavoritesTab: undefined;
-};
-const Tabs = createBottomTabNavigator<RootTabScreens>();
+import RootTabsNavigator from "./navigators/RootTabsNavigator";
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <NavigationContainer>
         <StatusBar style="auto" />
-        <Tabs.Navigator>
-          <Tabs.Screen
-            name="HomeTab"
-            component={HomeScreen}
-            options={{
-              headerShown: false,
-              tabBarIcon: (props) => (
-                <MaterialIcons
-                  name="home"
-                  size={props.size}
-                  color={props.color}
-                />
-              ),
-            }}
-          />
-          <Tabs.Screen
-            name="FavoritesTab"
-            component={FavoritesScreen}
-            options={{
-              title: "Favorites",
-              tabBarIcon: (props) => (
-                <MaterialIcons
-                  name="favorite-outline"
-                  size={props.size}
-                  color={props.color}
-                />
-              ),
-            }}
-          />
-        </Tabs.Navigator>
+        <RootTabsNavigator />
       </NavigationContainer>
     </SafeAreaProvider>
   );
