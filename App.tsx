dee1f6296a7cd64d7f873ff9f996a931dd5ebcb8
Elin Nyman <elin.nyman87@gmail.com>
2023-09-24 17:55:57 +0200

    Setting up navigation with the thursday and fridays lectures in mind

--- App.tsx
+++ App.tsx
@@ -1,20 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+
+function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>HOME</Text>
+    </View>
+  );
+}
+function FavScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>FAV</Text>
+    </View>
+  );
+}
+
+type RootTabScreens = {
+  Home: undefined;
+  Favorites: undefined;
+};
+const Tabs = createBottomTabNavigator<RootTabScreens>();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <StatusBar style="auto" />
+        <Tabs.Navigator>
+          <Tabs.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{
+              tabBarIcon: (props) => (
+                <MaterialIcons
+                  name="home"
+                  size={props.size}
+                  color={props.color}
+                />
+              ),
+            }}
+          />
+          <Tabs.Screen
+            name="Favorites"
+            component={FavScreen}
+            options={{
+              tabBarIcon: (props) => (
+                <MaterialIcons
+                  name="favorite-outline"
+                  size={props.size}
+                  color={props.color}
+                />
+              ),
+            }}
+          />
+        </Tabs.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
